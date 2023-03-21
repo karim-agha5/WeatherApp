1e192d90afc5919fe073a8b1c504228062ac5af6
karim <karim_agha5@hotmail.com>
2023-03-21 22:50:44 +0200

    add data binding

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,46 +3,29 @@ package com.example.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
+
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.GravityCompat
 import androidx.core.view.WindowCompat
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentStatePagerAdapter
-import androidx.viewpager.widget.ViewPager
 import com.bumptech.glide.Glide
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.tabs.TabLayout
+import com.example.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var ivImage: ImageView
-    lateinit var tvWeatherStatus: TextView
-    lateinit var toolbar: Toolbar
-    lateinit var collapsingToolbar: CollapsingToolbarLayout
-    lateinit var drawerLayer: DrawerLayout
-    lateinit var tabLayout: TabLayout
-    lateinit var viewPager: ViewPager
-    lateinit var viewPagerAdapter: ViewPagerAdapter
+    lateinit var mainActivityBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         // Set the status bar to be transparent
         WindowCompat.setDecorFitsSystemWindows(window,false)
-        setContentView(R.layout.activity_main)
 
-        initUI()
+        initMainActivityBinding()
         loadCollapsingToolbarImage()
         setupSupportActionBar()
-
-        viewPagerAdapter =
-            ViewPagerAdapter(supportFragmentManager, FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
-        viewPager.adapter = viewPagerAdapter
-        tabLayout.setupWithViewPager(viewPager)
-
-
+        setUpTabLayoutFunctionality()
 
     }
 
@@ -50,21 +33,19 @@ class MainActivity : AppCompatActivity() {
         Glide
             .with(this)
             .load(ResourcesCompat.getDrawable(resources,R.drawable.pxfuel,theme))
-            .into(ivImage)
+            .into(mainActivityBinding.ivCollapsingImage)
     }
 
-    private fun initUI(){
-        ivImage = findViewById(R.id.iv_collapsing_image)
-        toolbar = findViewById(R.id.toolbar)
-        drawerLayer = findViewById(R.id.drawer_layout)
-        collapsingToolbar = findViewById(R.id.collapsing_toolbar)
-        tvWeatherStatus = findViewById(R.id.tv_weather_status)
-        tabLayout = findViewById(R.id.tab_layout)
-        viewPager = findViewById(R.id.view_pager)
+    private fun initMainActivityBinding(){
+        mainActivityBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        mainActivityBinding.lifecycleOwner = this
     }
 
+    /*
+    * Sets the custom toolbar as the Appbar along with its components
+    * */
     private fun setupSupportActionBar(){
-        setSupportActionBar(toolbar)
+        setSupportActionBar(mainActivityBinding.toolbar)
         val supportActionBar = supportActionBar
         supportActionBar?.setHomeAsUpIndicator(R.drawable.round_menu_24)
         supportActionBar?.setDisplayShowHomeEnabled(true)
@@ -73,16 +54,27 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
+    private fun setUpTabLayoutFunctionality(){
+        mainActivityBinding.viewPager.adapter =
+            ViewPagerAdapter(supportFragmentManager, FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
+        mainActivityBinding.tabLayout.setupWithViewPager(mainActivityBinding.viewPager)
+
+    }
+
+    /*
+    * Handles the hamburger icon clicks
+    * */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         if(item.itemId == android.R.id.home){
-            if(drawerLayer.isDrawerOpen(GravityCompat.START)){
-                drawerLayer.closeDrawer(GravityCompat.START)
+            if(mainActivityBinding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+                mainActivityBinding.drawerLayout.closeDrawer(GravityCompat.START)
             }
             else{
-                drawerLayer.openDrawer(GravityCompat.START)
+                mainActivityBinding.drawerLayout.openDrawer(GravityCompat.START)
             }
         }
+
 
         return super.onOptionsItemSelected(item)
     }
