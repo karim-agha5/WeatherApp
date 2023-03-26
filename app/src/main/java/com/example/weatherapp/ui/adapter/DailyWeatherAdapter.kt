25e68605cde0b13e1533dd0162bce59b74b4498d
karim <karim_agha5@hotmail.com>
2023-03-26 19:26:21 +0200

    add home screen daily weather forecast enhancements

--- app/src/main/java/com/example/weatherapp/ui/adapter/DailyWeatherAdapter.kt
+++ app/src/main/java/com/example/weatherapp/ui/adapter/DailyWeatherAdapter.kt
@@ -14,6 +14,7 @@ import com.example.weatherapp.R
 import com.example.weatherapp.data.source.remote.response.daily.DailyWeatherInfo
 import com.example.weatherapp.databinding.DailyWeatherItemBinding
 import com.example.weatherapp.getCDegreeFormat
+import com.example.weatherapp.getDisplayedDate
 import com.example.weatherapp.getWeekDay
 import java.util.*
 
@@ -31,10 +32,26 @@ class DailyWeatherAdapter(
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         holder.binding.dailyWeatherInfo = getItem(position)
-        val dt = holder.binding.dailyWeatherInfo?.dt?.times(1000)
-        holder.binding.tvDailyWeatherItemDay.text = getWeekDay(dt ?: -1)
+        displayDate(holder,position)
+        // Display the weather degree in celsius format
         holder.binding.tvDailyWeatherItemDegree.text =
             getCDegreeFormat(Math.round((getItem(position).temp.day - 273.15)).toFloat())
+        loadWeatherIcon(holder)
+    }
+
+    // Displays everything related to the date .
+    // Day of the week, day of the month, month and current year
+    private fun displayDate(holder: CustomViewHolder,position: Int){
+        val dt = holder.binding.dailyWeatherInfo?.dt?.times(1000)
+        holder.binding.tvDailyWeatherItemDate.text = getDisplayedDate(dt ?: -1)
+        if(position == 0){
+            holder.binding.tvDailyWeatherItemDay.text = "Today"
+        }
+        else {
+            holder.binding.tvDailyWeatherItemDay.text = getWeekDay(dt ?: -1)
+        }
+    }
+    private fun loadWeatherIcon(holder: CustomViewHolder){
         if(holder.binding.dailyWeatherInfo != null){
             holder.binding.imageUrl = "https://openweathermap.org/img/wn/" +
                     holder.binding.dailyWeatherInfo?.weatherList?.get(0)?.icon+ "@2x.png"
